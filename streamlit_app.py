355a56d88d678ffa835975bd7ca317556d149322
Githubtien <142877437+Githubtien@users.noreply.github.com>
2023-08-23 11:13:19 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ from streamlit_option_menu import option_menu
 
 def Cham_ptn_qua_camera(selected):
     st.subheader(":red["+selected+"]")
-    img_file_buffer = st.camera_input("Take a picture")
+    img_file_buffer = st.camera_input("Take a picture",key='cmrtien')
 
     if img_file_buffer is not None:
         # To read image file buffer with OpenCV:
