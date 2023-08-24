4646e2637715a571a26cd103bac451006c87ddb1
Githubtien <142877437+Githubtien@users.noreply.github.com>
2023-08-24 21:47:57 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,34 @@
-# -*- coding: utf-8 -*-
+import cv2
 import streamlit as st
-from PIL import Image
-from PIL import ImageGrab  
-from PIL import ImageOps
-import random
+import numpy as np
 import os
-import cv2 
-import numpy as np 
+import pickle
 from streamlit_option_menu import option_menu
+from PIL import Image
+
+def Cham_ptn_qua_camera(selected):
+    st.subheader(":blue["+selected+"]")
+    dic_dap_an, ch_da = lay_dic_dap_an()
+    if ch_da == '':
+        st.write(":red[Chưa cung cấp đáp án!]")
+    else:    
+        st.write(ch_da)
+
+    img_file_buffer = st.camera_input("Chụp Phiếu Trắc Nghiệm",key='CPTNQC')
+
+    if img_file_buffer is not None:
+        # To read image file buffer with OpenCV:
+        bytes_data = img_file_buffer.getvalue()
+        cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+        #st.write('ban vua bat anh nay: ')
+        #Xuli_cv2_img_take(cv2_img)
+        # Check the type of cv2_img:
+        # Should output: <class 'numpy.ndarray'>
+        st.image(cv2_img, 'anh cv2 vua bat')
+
+        # Check the shape of cv2_img:
+        # Should output shape: (height, width, channels)
+        #st.write(cv2_img.shape)    
 
 def Xem_txtmark_hdan(selected):
     st.subheader(":blue["+selected+"]")
@@ -26,38 +47,88 @@ def Xem_txtmark_hdan(selected):
     '''
     st.markdown(txtmark)
 
-def Cham_ptn_qua_camera(selected):
-    st.subheader(":red["+selected+"]")
-    img_file_buffer = st.camera_input("Take a picture")
 
+def Cham_ptn_vanhien_da(cv2_img):
+    dic_dap_an = {}
+    return dic_dap_an
+
+def get_ten_file():
+    from datetime import datetime
+    # datetime object containing current date and time
+    now = datetime.now()
+    # dd/mm/YY H:M:S
+    dt_string = now.strftime("%Y/%m/%d %H:%M:%S")
+    ch=dt_string.replace('/','_')
+    ch=ch.replace(' ','_')
+    ch=ch.replace(':','')
+    return ch + '.jpg'
+
+def Cung_cap_da(selected):
+    global dic_dap_an, ch_dap_an
+    st.subheader(":red["+selected+"]")
+    img_file_buffer = st.camera_input("Chụp Phiếu Trắc Nghiệm Đáp Án",key='CCDA')
     if img_file_buffer is not None:
         # To read image file buffer with OpenCV:
         bytes_data = img_file_buffer.getvalue()
         cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-        #st.write('ban vua bat anh nay: ')
-        #Xuli_cv2_img_take(cv2_img)
-        # Check the type of cv2_img:
-        # Should output: <class 'numpy.ndarray'>
-        st.image(cv2_img, 'anh cv2 vua bat')
+        #paper = cham_ptn_vanhien(cv2_img,dic_dap_an)
+        dic_dap_an = Cham_ptn_vanhien_da(cv2_img)
+        if dic_dap_an != {}:
+            ch_dap_an = ''
+            for keyd in dic_dap_an:
+                if  keyd < len(dic_dap_an) - 1 :  
+                    ch_dap_an = ch_dap_an + str(keyd+1) + dic_dap_an[keyd] + ', '
+                else:    
+                    ch_dap_an = ch_dap_an + str(keyd+1) + dic_dap_an[keyd]
+            st.write(ch_dap_an)
+            chononoffluuda=st.radio("", ('Không lưu (KL)', 'Xac nhan luu (OK)') ,horizontal=True,key='luuda')
+            if  'OK' in chononoffluuda:
+                tepluub = get_ten_file()
+                tepluub = tepluub.replace('.jpg','dap_an_'+made+'.pkl')
+                with open(tepluub, 'wb') as fwb:
+                    ldap_an=[dic_dap_an,ch_dap_an]
+                    pickle.dump(ldap_an, fwb)
+                    st.write('Đã lưu đáp án!')    
+                    #return ch_dap_an
+            else:
+                st.write('Cung cấp đáp án chưa thành công!')
 
-        # Check the shape of cv2_img:
-        # Should output shape: (height, width, channels)
-        #st.write(cv2_img.shape)    
+###############################
 
-##########################################################
-st.title("Chấm Điểm Trên Phiếu Trắc Nghiệm với Streamlit")
+def lay_dic_dap_an():
+    dic_dap_an={}
+    ch_da = ''
+    listd = os.listdir('./dap_an/')
+    for tep in listd:
+        if 'dap_an_' + '001' + '.pkl' in tep:
+            st.write(":red[Chấm theo "+tep[17: -4]+" :]")
+            tepluuda = 'dap_an/'+tep
+            with open(tepluuda, 'rb') as fwb:
+                ldata = pickle.load(fwb)
+                dic_dap_an = ldata[0]
+                ch_da = ldata[1]
+                #st.write(dic_dap_an)
+                #st.write(ch_da)
+    return dic_dap_an,ch_da
+
+################################################################################################
+# main()
+st.title("Chấm Điểm Trên Phiếu Trắc Nghiệm Bằng Camera")
 
 with st.sidebar:
-    selected = option_menu("Main Menu", ["1. Cung cấp đáp án", "2. Upload Phiếu trắc nghiệm cho máy chấm",  
-                                         "3. Chấm qua Camera màn hình", "4. Hướng dẫn","5. About" ], default_index=0)
-if '3.' in selected:
+    selected = option_menu("Main Menu", ["1. Cung cấp đáp án",   
+                                         "2. Chấm PTN qua Camera", "3. Hướng dẫn","4. About" ], default_index=1)
+if '1.' in selected:
+    ch_dap_an = Cung_cap_da(selected)    
+    st.write(ch_dap_an)
+
+elif '2.' in selected:
     Cham_ptn_qua_camera(selected)
 
-elif '4.' in selected:
+elif '3.' in selected:
     Xem_txtmark_hdan(selected)
 else:
     st.subheader(":orange[5. About]")
     st.write('App này do tiengs89@gmail.com làm thử năm 2023 để giúp Gv')
+###################################
 
-
-exit()
