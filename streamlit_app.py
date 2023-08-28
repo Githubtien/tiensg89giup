7eaf2adb513717343619854ee59947c885ed6353
Githubtien <142877437+Githubtien@users.noreply.github.com>
2023-08-28 12:05:33 +0700

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+# reboot app : https://share.streamlit.io/
 #lenh chay
 #streamlit run streamlit_app.py
 #pip install streamlit-camera-input-live
@@ -11,9 +12,39 @@ from PIL import Image
 from PIL import ImageOps
 #from PIL import ImageGrab  
 import imutils
-from imutils.perspective import four_point_transform
+#from imutils.perspective import four_point_transform
 
 ##########################
+def order_points(pts):
+	rect = np.zeros((4, 2), dtype = "float32")
+	s = pts.sum(axis = 1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+	diff = np.diff(pts, axis = 1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+	# return the ordered coordinates
+	return rect
+
+def four_point_transform(image, pts):
+	rect = order_points(pts)
+	(tl, tr, br, bl) = rect
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+	dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+	# compute the perspective transform matrix and then apply it
+	M = cv2.getPerspectiveTransform(rect, dst)
+	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+	# return the warped image
+	return warped
 
 def get_x_ver0(s):
    s = cv2.boundingRect(s)
